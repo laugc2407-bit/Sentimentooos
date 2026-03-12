2eb31146fa99dbb632562c64527019ad4836b7f5
laugc2407-bit <laugc2407@gmail.com>
2026-03-12 10:32:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,18 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
+             with open('Cat playing animation.json') as source:
+     animation=json.load(source)
+st.lottie(animation,width =350)
+
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+with open('Cat playing animation.json') as source:
+     animation=json.load(source)
+st.lottie(animation,width =350)
+
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+             with open('Cat playing animation.json') as source:
+     animation=json.load(source)
+st.lottie(animation,width =350)
